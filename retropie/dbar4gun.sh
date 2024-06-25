103b2306cc6a5c24bb6992b22373c7ebd486afdc
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2024-06-25 15:12:42 +0200

    update support retropie

--- retropie/dbar4gun.sh
+++ retropie/dbar4gun.sh
@@ -14,6 +14,7 @@ rp_module_desc="dbar4gun is a Linux userspace driver for the DolphinBar."
 rp_module_help="dbar4gun dvr from https://github.com/lowlevel-1989/dbar4gun"
 rp_module_licence="MIT https://raw.githubusercontent.com/lowlevel-1989/dbar4gun/master/LICENSE"
 rp_module_repo="git https://github.com/lowlevel-1989/dbar4gun master"
+rp_module_section="exp"
 rp_module_section="driver"
 
 function depends_dbar4gun() {
